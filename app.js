5116d4e325aaea25c7ae3519ddd7485bd5c301dc
ibrahim mulei <ibrahimmulei@gmail.com>
2024-08-01 12:44:17 +0300

    created a booking route and implemented all crud functionalities for it for the bookings and a booking with id

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 
 const app = express();
 const eventRoutes = require("./api/routes/events");
+const bookingRoutes = require("./api/routes/bookings");
 
 app.use("/events", eventRoutes);
+app.use("/bookings", bookingRoutes);
 
 module.exports = app;
