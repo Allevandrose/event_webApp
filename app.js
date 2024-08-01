422240658140ee95cd1cb5aef6ec11e836a593bc
ibrahim mulei <ibrahimmulei@gmail.com>
2024-08-01 14:48:42 +0300

    installed the morgan node package set it up for logging all request to the server and also created error handling middlewares

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 const eventRoutes = require("./api/routes/events");
 const bookingRoutes = require("./api/routes/bookings");
 
+app.use(morgan("dev"));
+
 app.use("/events", eventRoutes);
 app.use("/bookings", bookingRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    Error: error.message,
+  });
+});
 
 module.exports = app;
